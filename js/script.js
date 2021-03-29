8ae44632a88f6b878decfb9e19c5dc38f9557964
parisa <parisazangiabadi0@gmail.com>
2021-03-29 22:41:28 +0430

    add validation form

--- js/script.js
+++ js/script.js
@@ -1,103 +1,142 @@
-// jQuery.validator.setDefaults({
-//     debug: true,
-//     success: "valid"
-//   });
+jQuery.validator.setDefaults({
+    debug: true,
+    success: "valid"
+  });
  
-//   $( "#myform-id" ).validate({
-//       rules: {
-//         Uname: {
-//         required: true,
-//         rangelength: [3, 10]
-//       },
+  $( "#myform-id" ).validate({
+      rules: {
+        Uname: {
+        required: true,
+        
+      },
       
-//       Password: {
-//         required: true,
-//         minlength: 5
-//     },
-//     },
-
-//     messages: {
-//         name: "فیلد نام وارد کن ",
-//         email: { required:"فیلد ایمیل را وارد کن",
-//                    email:"ایمیل را درست وارد کن"
-//     },
-//         age: {
-//             required: "فیلد سن را وارد کن",
-//             number: "داده را بصورت عددی وارد کن"
-//         },
-//     }
-//   });
-
-
-
-   
-// function functionValid(){
-//     var test =$("#myform").valid();
-//     console.log(test);
-    
-//     if (test === true){
-//         var frm = $('#myform');
-        
-//         frm.submit(function (e) {
-//         $.ajax({
-//             url: 'https://api.noaeincloud.ir/api/sample',
-//             data:frm.serialize(),
-//             type: 'GET',
-//             dataType: 'JSON',
-//             beforeSend: function() {
-          
-//             },
-//             success: function(xhr, response) {
-                
-//                 swal({
-//                     title: "it is registered !",
-//                     text: "You clicked the button!",
-//                     icon: "success",
-//                     button: "Aww yiss!",
-//                   });
-//             },
-//             error: function(xhr, status, error) {
-              
-//                 swal ( xhr.message ,  "Something went wrong!" ,  "error" )
-//             },
-         
-//           });
-        
-//     })
-//     }
-    
-//     }
-
-function functionValidate(){
+      Password: {
+        required: true,
+        minlength: 6
+      
+    },
+    },
+ messages:{},
+ errorPlacement: function(error,element) {
+  return true;
+}
+//  errorElement : 'div',
+//  errorLabelContainer: ''
+    // messages: {
+    //     name: "فیلد نام وارد کن ",
+    //     email: { required:"فیلد ایمیل را وارد کن",
+    //                email:"ایمیل را درست وارد کن"
+    // },
+    //     age: {
+    //         required: "فیلد سن را وارد کن",
+    //         number: "داده را بصورت عددی وارد کن"
+    //     },
+    // }
+  });
  
-  var error;
-  error = document.getElementById("message-error-id");
 
-  var UserName = document.forms["myform-id"]["Uname"].value;
-  var Password = document.forms["myform-id"]["Password"].value;
-  
-  if ( UserName == "" && Password == "" ){
    
+function functionValid(){
+    var test =$("#myform-id").valid();
+    console.log(test);
+    
+    if (test === true){
+        var frm = $('#myform-id');
+        
+        frm.submit(function (e) {
+        $.ajax({
+            url: 'https://api.noaeincloud.ir/api/sample',
+            data:frm.serialize(),
+            type: 'GET',
+            dataType: 'JSON',
+            beforeSend: function() {
+          
+            },
+            success: function(xhr, response) {
+                console.log(xhr)
+              if(xhr.status == true){
+                alert("go page dash");
+              }
+              else{
+                $("#message-id").css('display','block') ;
+              }
+            
+              
+              // swal({
+                //     title: "it is registered !",
+                //     text: "You clicked the button!",
+                //     icon: "success",
+                //     button: "Aww yiss!",
+                //   });
+            },
+            error: function(xhr, status, error) {
+              
+                // swal ( xhr.message ,  "Something went wrong!" ,  "error" )
+                alert('no')
+            },
+         
+          });
+        
+    })
+    }
+    
+     }
 
-    document.getElementById("user-id").style.borderColor = "#4964ff";
-document.getElementById("pass-id").style.borderColor = "#4964ff";
-// alert("no")
-   return false;
-  }
+// function functionValidate(){
   
-  else if ( UserName != "" && Password == "" ){
+//   var  formData = {
+//   UserName : $('#user-id').val(),
+//   Password :  $('#pass-id').val(),
+//   };
+  
+
+//  if ( formData.UserName == "" && formData.Password == "" ){
+   
+//    $('#user-id').css('borderColor' , 'red');
+//    $('#pass-id').css('borderColor', 'red');
+//    return false;
+//   }
+  
+//   else if ( formData.UserName != "" && formData.Password == "" ){
+//     $('#pass-id').css('borderColor' ,'red');
+//     $('#user-id').css('borderColor' , '#3d55db');
+//     return false;
+//   }
+  
+//   else if ( formData.UserName == "" && formData.Password != "" ){
+//     $('#user-id').css('borderColor','red') ;
+//     $('#pass-id').css('borderColor' , '#3d55db');
+//     return false;
+//   }
+
+//   else {
+//     $('#user-id').css('borderColor' , '#3d55db');
+//     $('#pass-id').css('borderColor', '#3d55db');
+//     // alert('yes');
     
+//     $.ajax({
+//       url: 'https://api.noaeincloud.ir/api/sample',
+//       data: formData,
+//       type: 'GET',
+//       dataType: 'JSON',
+//       beforeSend: function() {
+//     },
     
-document.getElementById("pass-id").style.borderColor = "#4964ff";
-  // alert("pass")
-   return false;
-  }
+//       success: function(xhr, response) {
+//         console.log(xhr);
+//         if(xhr.status == false){
+//             alert("parisa true")
+//           }
+         
+//       },
+//       error: function(xhr, status, error) {
+//         console.log("is error",xhr);
+        
+//           // swal ( xhr.message ,  "Something went wrong!" ,  "error" )
+//       },
+   
+//     });
+//   }
   
-  else if ( UserName == "" && Password != "" ){
-    document.getElementById("user-id").style.borderColor = "#4964ff";
-//  alert("user")
-   return false;
-  }
-  
- }
+//  }
  
